0fddb5bc73901353d87e212707042e07dad040b4
NishantPlayWork <147664351+NishantPlayWork@users.noreply.github.com>
2024-11-22 15:04:12 -0800

    Update App.java

--- src/main/java/com/mkyong/hashing/App.java
+++ src/main/java/com/mkyong/hashing/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! using Self Hosted Runner 1-- yay" );
+        System.out.println( "Hello World! using Github Hosted Runner 1-- yay" );
     }
 }
